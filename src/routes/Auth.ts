6dd2e7849c0c102b1712ba67fb8c802bd187515b
TrueFirel <elekid71@gmail.com>
2019-02-26 02:32:08 +0300

    fixed auth route

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -1,9 +1,11 @@
+import Config from "../app/Config";
 import DBProcessor from "../app/DBProcessor";
 import MessageClient from "../app/MessageClient";
 import UserControllerWrapper from "../controllers/User/UserController";
+import AWSConnector from "../util/AWSConnector";
 
-export default function(dbProcessor: DBProcessor, messageClient: MessageClient) {
+export default function(dbProcessor: DBProcessor, messageClient: MessageClient, config: Config) {
 
-    const UserController = UserControllerWrapper(dbProcessor, messageClient);
+    const UserController = UserControllerWrapper(dbProcessor, messageClient, new AWSConnector(config));
     this.post("/login", UserController.loginAccount);
 }
