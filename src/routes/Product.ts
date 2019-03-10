d520827d353816906bcc862afd635781e158f520
TrueFirel <elekid71@gmail.com>
2019-03-10 20:26:23 +0300

    Product fix

--- src/routes/Product.ts
+++ src/routes/Product.ts
@@ -2,11 +2,13 @@ import DBProcessor from "../app/DBProcessor";
 import ProductControllerWrapper from "../controllers/Product/ProductController";
 import UserReviewControllerWrapper from "../controllers/User/UserReviewController";
 import CheckAuth from "../middleware/CheckAuth";
+import AWSConnector from "../util/AWSConnector";
 
 export default function(dbProcessor: DBProcessor) {
 
     const checkAuth = CheckAuth(dbProcessor);
-    const ProductController = ProductControllerWrapper(dbProcessor);
+    const awsConnector = new AWSConnector({});
+    const ProductController = ProductControllerWrapper(dbProcessor, awsConnector);
     const UserReviewController = UserReviewControllerWrapper(dbProcessor);
 
     this.get("/product/:id", checkAuth.isAnyAuth, ProductController.getProduct);
